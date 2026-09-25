82dcdb927e05c5eaa7e2b2fdd229925842513a5b
Harol Ismael Rodriguez Romero <rharol060@gmail.com>
2026-09-25 09:20:39 -0600

    Subir examen

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Examen_HIRR</title>
+  </head>
+  <body>
+    <header>
+    <?php include_once("public/encabezado.php")?>
+     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</header>
+   
+   <h1>Bienvenido a la pagina de Harol Ismael Rodriguez Romero</h1>
+  </body>
+  <footer>
+    <?php include_once("public/pie.php")?>
+</footer>
+</html>
